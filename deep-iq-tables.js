0e885ee41f2843eac8764bcf159ec01f84a2ab57
Therealajahn <therealajahn@gmail.com>
2025-11-11 00:54:33 -0600

    building out  data structure of the tables

--- deep-iq-tables.js
+++ deep-iq-tables.js
@@ -1,11 +1,39 @@
 const deepIQTables = {
-	"1":[],
-	"2":[],
-	"3":[],
-	"4":[],
-	"5":[],
-	"6":[],
-	"7":[],
+	"1":{
+		table: [
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			"Bury your best creature (you decide)",
+			{
+				text:"Put 1/1 creature into play",
+				baseStats:[1,1],
+				roll:"token",
+				modifier: -4,
+			},
+		],
+		advancement: 9,
+	},
+	"2":{
+		table: [
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			"Do nothing",
+			{
+				text:"Put 2/2 creature into play",
+				baseStats:[2,2],
+				roll:"token",
+				modifier: 0,
+			},
+		],
+		advancement: 9,
+	},
 	"spooky":[],
 	"token":[],
 }
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(deepIQTables);
+function roll(){
+	return Math.floor(Math.random() * multiplier);
+};
+console.log("roll:",roll(10));
