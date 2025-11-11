cd4a5aa3068a745c821e974982b2da60866bbeaf
Therealajahn <therealajahn@gmail.com>
2025-11-11 01:19:44 -0600

    started

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ console.log(deepIQTables);
 function roll(){
 	return Math.floor(Math.random() * multiplier);
 };
-console.log("roll:",roll(10));
+//it gets the type:
+//String: either a Do nothing or a user action 
+//Array: a series of actions to be handled recursively
+//Object: an action for the app to change some data or make a roll 
+//...on another table
